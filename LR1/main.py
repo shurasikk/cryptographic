a4e18e5d4d15fa3fdb6ce6c88ae82073f2cc58d2
shurasikk <90204027+shurasikk@users.noreply.github.com>
2024-02-21 22:48:27 +0300

    первообразные корни
    с этапами

--- LR1/main.py
+++ LR1/main.py
@@ -8,10 +8,18 @@ def is_prime(n):
         i+=1
     return 1
 
+def nod(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
 def div_list(n):
     i=2
     list = []
-    while i<=sqrt(n):
+    while i <= n/2:
         if is_prime(i) and n%i==0:
             list.append(i)
         i+=1
@@ -26,5 +34,41 @@ def fi(n):
         p*=1-(1/l[i])
     return int(p)
 
+n = int(input("Введите число n, для которого найдём первообразные корни: "))
 
-print(fi(63))
+k = fi(fi(n))
+l = div_list(fi(n))
+print("1-й этап: Количество корней = " + str(k))
+
+s = []
+for i in range(len(l)):
+    s.append(int(fi(n)/l[i]))
+
+print("2-й этап: Будем возводить взаимно простые остатки в степени " + str(s))
+
+cur = 0
+i = 2
+ans=[]
+while i < n and cur<k:
+    flag = 1
+    print(str(i + 1) + "-й этап: ")
+    if nod(n,i) == 1:
+        for alph in range(len(s)):
+
+            print(str(i) + "^^" + str(s[alph]) + " \u2261 " + str((i ** s[alph]) % n) + " mod " + str(n))
+
+            if (i ** s[alph]) % n == 1:
+                flag = 0
+                print("Число не корень.")
+                i+=1
+                break
+        if flag == 1:
+            print("Число является корнем.")
+            ans.append(i)
+            cur+=1
+            i+=1
+    else:
+        print("Число " + str(i) + " не является первообразным корнем, так как оно не взаимно простое с " + str(n))
+        i+=1
+
+print("Первообразными корнями являются числа " + str(ans))
